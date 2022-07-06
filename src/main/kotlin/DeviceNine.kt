bc6daf9e8024a64665b6b499fc809e5f9040b16b
DazeCake <dazecake@gmail.com>
2022-07-06 17:29:46 +0800

    Init project

---
+++ src/main/kotlin/DeviceNine.kt
@@ -0,0 +1,20 @@
+package moe.dazecake
+
+import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
+import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.utils.info
+
+object DeviceNine : KotlinPlugin(
+    JvmPluginDescription(
+        id = "moe.dazecake.DeviceNine",
+        name = "DeviceNine",
+        version = "1.0-SNAPSHOT",
+    ) {
+        author("DazeCake")
+        info("""明日方舟速通云控机器人""")
+    }
+) {
+    override fun onEnable() {
+        logger.info { "Plugin loaded" }
+    }
+}
